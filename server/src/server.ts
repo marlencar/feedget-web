61a24452121eb974da987db289bad57345c53706
marlencar <alencar.marcelo1997@gmail.com>
2022-05-08 10:32:25 -0300

    Trying to fix CORS error

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 app.listen(process.env.PORT || 3333, () => {
   console.log("HTTP server running!");
